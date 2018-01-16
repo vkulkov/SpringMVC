bbe6228b48a70af60d4a06c4a0f0987d9ea5b827
Valera <kulkowalery@gmail.com>
2018-01-17 01:25:05 +0200

    Spitter controller implementation

---
+++ src/main/java/com/springinaction/spitter/mvc/SpitterController.java
@@ -0,0 +1,31 @@
+package com.springinaction.spitter.mvc;
+
+import com.springinaction.spitter.domain.Spitter;
+import com.springinaction.spitter.service.SpitterService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.inject.Inject;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+@RequestMapping("/spitter")
+public class SpitterController {
+    private SpitterService spitterService;
+
+    @Inject
+    public SpitterController(SpitterService spitterService) {
+        this.spitterService = spitterService;
+    }
+
+    @RequestMapping(value = "/spittles", method = GET)
+    public String listSpittlesForSpitter(@RequestParam("spitter") String username, Model model) {
+        Spitter spitter = spitterService.getSpitter(username);
+        model.addAttribute(spitter);
+        model.addAttribute(spitterService.getSpittlesForSpitter(username));
+        return "spittles/list";
+    }
+}
